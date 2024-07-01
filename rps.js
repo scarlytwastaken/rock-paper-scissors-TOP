aa813bef92507c2befe028867a4cb09b2948332e
scarlytwastaken <173748550+scarlytwastaken@users.noreply.github.com>
2024-07-01 05:07:17 -0700

    Create functions for random number and RPS choice
    Function to generate a random nunmber between 1 and 3 inclusive, and
    assign integers 1, 2, and 3 to RPS choices. Log integer selection and
    RPS choice assignment to make sure code is working.

--- rps.js
+++ rps.js
@@ -1,1 +1,21 @@
-console.log('AYO?');
+function getRandoIntInclusive(min, max) {
+    const minCiel = Math.ceil(min);
+    const maxFloor = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloor - minCiel + 1) + minCiel);
+}
+
+choiceNum = getRandoIntInclusive(1,3);
+
+console.log(choiceNum);
+
+function getComputerChoice(choiceNum) {
+    if (choiceNum === 1) {
+        console.log('rock');
+    } else if (choiceNum === 2) {
+        console.log('paper');
+    } else if (choiceNum === 3) {
+        console.log('scissors');
+    } else console.log('ERRORRRRR');
+}
+
+console.log(getComputerChoice(choiceNum));
