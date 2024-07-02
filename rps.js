d0e36432c7d74088d3b5b5121183fb40e9b37003
scarlytwastaken <173748550+scarlytwastaken@users.noreply.github.com>
2024-07-01 17:24:15 -0700

    Create func to play 1 round of rps
    Use all previous function inside new func to play 1 round of RPS.
    Use if, elseif, else statements nested inside if statement to set up
    logic for when player plays specific hand vs computer. bug test and
    ensure playRound returns expected values in console.

--- rps.js
+++ rps.js
@@ -4,30 +4,44 @@ function getRandoIntInclusive(min, max) {
     return Math.floor(Math.random() * (maxFloor - minCiel + 1) + minCiel);
 }
 
-choiceNum = getRandoIntInclusive(1,3);
-
-console.log(choiceNum);
-
 function getComputerChoice(choiceNum) {
     if (choiceNum === 1) {
-        console.log('rock');
+        return 'rock';
     } else if (choiceNum === 2) {
-        console.log('paper');
+        return 'paper';
     } else if (choiceNum === 3) {
-        console.log('scissors');
-    } else console.log('ERRORRRRR');
+        return 'scissors';
+    } else return 'ERRORRRRR';
 }
-
-console.log(getComputerChoice(choiceNum));
 
 function getHumanChoice () {
     return prompt('Enter: rock, paper, or scissors.');
 }
 
-humanChoice = getHumanChoice();
-
-console.log(humanChoice);
-
 humanScore = 0
 
 computerScore = 0
+
+function playRound () {
+    choiceNum = getRandoIntInclusive(1,3);
+    console.log(choiceNum);
+    computerChoice = getComputerChoice(choiceNum)
+    console.log(computerChoice);
+    humanChoice = getHumanChoice();
+    console.log(humanChoice);
+
+    
+
+    if (humanChoice === 'rock') {
+        console.log('You played rock');
+        if (computerChoice === 'rock') {
+            return "Tie! Rock ties rock.";
+        } else if (computerChoice === 'paper') {
+            return "You Lose! Paper beats rock.";
+        } else if (computerChoice === 'scissors') {
+            return "You win! Rock beats scissors."
+        } else {
+            return "Invalid entry."
+        }
+    }
+}
