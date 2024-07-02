a99cf360b19f472ccaafed5f90804e9085723442
scarlytwastaken <173748550+scarlytwastaken@users.noreply.github.com>
2024-07-01 17:44:09 -0700

    Complete playRound function with last 2 RPS options
    Fill out playRound fun with paper and scissors player options and
    resulting logic against computer choices. Bug test func. Refine console
    output order.

--- rps.js
+++ rps.js
@@ -23,25 +23,48 @@ humanScore = 0
 computerScore = 0
 
 function playRound () {
-    choiceNum = getRandoIntInclusive(1,3);
-    console.log(choiceNum);
-    computerChoice = getComputerChoice(choiceNum)
-    console.log(computerChoice);
     humanChoice = getHumanChoice();
-    console.log(humanChoice);
-
-    
+    console.log(`You played: ${humanChoice}.`);
+    choiceNum = getRandoIntInclusive(1,3);
+    //console.log(choiceNum);
+    computerChoice = getComputerChoice(choiceNum)
+    console.log(`The opponent plays: ${computerChoice}.`);
+        
 
     if (humanChoice === 'rock') {
-        console.log('You played rock');
+        //console.log('You played rock.');
         if (computerChoice === 'rock') {
             return "Tie! Rock ties rock.";
         } else if (computerChoice === 'paper') {
-            return "You Lose! Paper beats rock.";
+            return "You Lose! Rock is defeated by paper.";
         } else if (computerChoice === 'scissors') {
             return "You win! Rock beats scissors."
         } else {
-            return "Invalid entry."
+            return "Error: computer choice."
         }
-    }
+    } else if (humanChoice === 'paper') {
+        //console.log('You played paper.');
+        if (computerChoice === 'rock') {
+            return "You win! Paper beats rock.";
+        } else if (computerChoice === 'paper') {
+            return "Tie! Paper ties paper.";
+        } else if (computerChoice === 'scissors') {
+            return "You lose! Paper is defeated by scissors."
+        } else {
+            return "Error: computer choice."
+        }
+    } else if (humanChoice === 'scissors') {
+        //console.log('You played scissors.');
+        if (computerChoice === 'rock') {
+            return "You lose! Scissors is defeated by rock.";
+        } else if (computerChoice === 'paper') {
+            return "You win! Scissors beats paper.";
+        } else if (computerChoice === 'scissors') {
+            return "Tie! Scissors ties scissors."
+        } else {
+            return "Error: computer choice."
+        }
+    } else {
+        return 'Your choice is not valid';
+    }   
 }
